e05e03c34fc5b086317fc180eb75eec17d743f1e
Shubham Verma <lpushubham98@gmail.com>
2021-08-17 12:04:52 +0530

    edfora

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cookieparser = require("cookie-parser");
+
+const PORT = 5050;
+
+//Routes
+const authRoutes = require("./routes/auth");
+
+//db Connections
+mongoose
+	.connect(process.env.DATABASE, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		console.log("DB CONNECTED");
+	});
+
+
+//App level Middleware
+app.use(express.json());
+app.use(cookieparser());
+
+
+//My Routes
+app.use("/api", authRoutes);
+
+
+app.listen(PORT, () => {
+  console.log(`App is Running at the Port:${PORT}`);
+})
---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router();
+const { check } = require("express-validator");
+const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
+
+router.post(
+	"/signup",
+	[
+		check("name")
+			.isLength({ min: 3 })
+			.withMessage("Name must be at least 3 chars long"),
+		check("email").isEmail().withMessage("Provide an Email"),
+		check("password").isLength({ min: 3 }).withMessage("must contain a number"),
+	],
+	signup
+);
+
+router.post(
+	"/signin",
+	[
+		check("email").isEmail().withMessage("Provide an Email"),
+		check("password").isLength({ min: 3 }).withMessage("Password Is Required"),
+	],
+	signin
+);
+
+router.get("/signout", signout);
+
+module.exports = router;
